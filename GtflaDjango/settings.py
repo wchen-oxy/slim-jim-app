68f343160639435cb259f6d934beebd23dd55022
William S Chen <blackjamx5@hotmail.com>
2019-02-26 20:25:33 -0800

    second

--- GtflaDjango/settings.py
+++ GtflaDjango/settings.py
@@ -129,22 +129,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Begin random shit for web deployment
 
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# The URL to use when referring to static files (where they will be served from)
-STATIC_URL = '/static/'
-
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
